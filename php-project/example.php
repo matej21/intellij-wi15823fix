0c2a73fa57c02ac4106ea53a1a95d5fc0733f1b7
David Matejka <matej21@matej21.cz>
2014-07-04 01:18:20 +0200

    init

---
+++ php-project/example.php
@@ -0,0 +1,18 @@
+<?php
+
+class Foo
+{
+
+    /**
+     * @param \stdClass description
+     * @param \stdClass $param2 description
+     *
+     */
+    public function doSomething($param1, $param2)
+    {
+
+    }
+}
+
+$foo = new Foo;
+$foo->doSomething();
---
+++ src/cz/matej21/intellij/wi15823fix/ParserDefinition.java
@@ -0,0 +1,32 @@
+package cz.matej21.intellij.wi15823fix;
+
+import com.intellij.lang.ASTNode;
+import com.intellij.psi.PsiElement;
+import com.intellij.psi.tree.IElementType;
+import com.jetbrains.php.lang.documentation.phpdoc.parser.PhpDocParser;
+import com.jetbrains.php.lang.documentation.phpdoc.parser.tags.PhpDocTagParserRegistry;
+import com.jetbrains.php.lang.documentation.phpdoc.psi.PhpDocPsiCreator;
+import com.jetbrains.php.lang.parser.PhpParserDefinition;
+import org.jetbrains.annotations.NotNull;
+
+
+public class ParserDefinition extends PhpParserDefinition{
+
+	@NotNull
+	@Override
+	public PsiElement createElement(ASTNode node) {
+
+		IElementType type = node.getElementType();
+		if(type == PhpDocPsiCreator.phpDocParam) {
+			return new MyPhpDocParamTag(node);
+		}
+		return super.createElement(node);
+	}
+
+	public ParserDefinition() {
+		super();
+		new PhpDocParser(); //initialize phpdoc parser registry
+		PhpDocTagParserRegistry.register("@param", new MyPhpDocParamTagParser());
+	}
+
+}
